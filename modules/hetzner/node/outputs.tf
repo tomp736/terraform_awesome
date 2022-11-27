4c935c28f78feb1753a17d804d6e69a07d9ef71b
tompisula <tompisula@labrats.work>
2022-11-27 15:56:53 +0100

    add output

--- modules/hetzner/node/outputs.tf
+++ modules/hetzner/node/outputs.tf
@@ -2,10 +2,17 @@ output "ipv4_address" {
   value     = hcloud_server.node.ipv4_address
   sensitive = true
 }
+
+output "id" {
+  value = hcloud_server.node.id
+  sensitive = true
+}
+
 output "name" {
   value     = local.hetzner.name
   sensitive = true
 }
+
 output "nodetype" {
   value = local.hetzner.nodetype
 }
