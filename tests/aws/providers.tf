06cf1d8ac0fc41df9302efa33c2fc21b424c6642
tompisula <tompisula@labrats.work>
2022-12-01 10:02:17 +0100

    aws changes

--- tests/aws/providers.tf
+++ tests/aws/providers.tf
@@ -9,7 +9,6 @@ terraform {
 }
 
 provider "aws" {
-  shared_config_files      = ["~/.aws/conf"]
+  shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "default"
 }
