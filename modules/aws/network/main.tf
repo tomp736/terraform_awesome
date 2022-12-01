7209944dcee8f98f556a0ddbaf2f0e191a8e7ce3
tompisula <tompisula@labrats.work>
2022-12-01 15:08:12 +0100

    fix aws

--- modules/aws/network/main.tf
+++ modules/aws/network/main.tf
@@ -61,12 +61,12 @@ resource "aws_security_group_rule" "ingress" {
   security_group_id = aws_vpc.default.default_security_group_id
 }
 
-resource "aws_security_group_rule" "egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-  security_group_id = aws_vpc.default.default_security_group_id
-}
+# resource "aws_security_group_rule" "egress" {
+#   type              = "egress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   ipv6_cidr_blocks  = ["::/0"]
+#   security_group_id = aws_vpc.default.default_security_group_id
+# }
