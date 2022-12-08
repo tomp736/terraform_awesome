417c2104310100911015b84ec9e693ac8af40a35
tompisula <tompisula@labrats.work>
2022-12-08 15:08:56 +0100

    fix merge

--- modules/hetzner/node/main.tf
+++ modules/hetzner/node/main.tf
@@ -15,7 +15,7 @@ resource "hcloud_server" "node" {
   dynamic "network" {
     for_each = var.networks
     content {
-      network_id = network.value
+      network_id = network.value.id
     }
   }
 
