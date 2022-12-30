68eb6cbe153767eb23afa5354e2d27df22ec98b7
tompisula <tompisula@labrats.work>
2022-12-30 10:27:19 +0100

    refactor for tests

--- tests/aws/node/variables.tf
+++ tests/aws/node/variables.tf
@@ -1,9 +1,3 @@
-variable "test_id" {
-  description = "(Required) - TestID for testing."
-  type        = string
-  sensitive   = true
-}
-
 variable "github_public_key" {
   description = "(Required) - Public key for testing."
   type        = string
