bf972c3f60a5418cfe89f84e3aa38dc782a88d2e
tompisula <tompisula@labrats.work>
2022-12-30 10:13:22 +0100

    remove node.state

--- tests/hetzner/node_group/locals.tf
+++ tests/hetzner/node_group/locals.tf
@@ -3,5 +3,5 @@
 locals {
   config          = jsondecode(file("files/config.json"))
   config_networks = { for network in local.config.networks : network.id => network }
-  config_nodes    = { for node in local.config.nodes : node.id => node if node.state == "enabled" }
+  config_nodes    = { for node in local.config.nodes : node.id => node }
 }
